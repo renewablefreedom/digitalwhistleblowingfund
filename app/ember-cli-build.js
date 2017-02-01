1f74847eb97aed5afbc261ea6b61ad93988f7190
moba <mo@localhost>
2017-02-02 00:15:03 +0100

    remove experimental imports, reset wrong namespace setting

--- app/ember-cli-build.js
+++ app/ember-cli-build.js
@@ -22,8 +22,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/moment/moment.js');
-  app.import('bower_components/moment-timezone/builds/moment-timezone-with-data.min.js');
 
   return app.toTree();
 };
