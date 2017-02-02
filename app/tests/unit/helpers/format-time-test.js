340de6a3e75779fe23a4f65a3ce4f256dfdf58f3
moba <mo@localhost>
2017-02-02 03:09:55 +0100

    Fixed format_time test

--- app/tests/unit/helpers/format-time-test.js
+++ app/tests/unit/helpers/format-time-test.js
@@ -1,12 +1,12 @@
 
-import { formatTime } from 'polly/helpers/format-time';
+import { format_time } from 'polly/helpers/format-time';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | format time');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = formatTime([42]);
+  let result = format_time([42]);
   assert.ok(result);
 });
 
