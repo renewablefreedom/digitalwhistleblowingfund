b2681b41b4face081f0dc872a2825778dec8dcc8
moba <moba@users.noreply.github.com>
2018-07-30 16:58:19 +0000

    initial fork for whistleblowing fund

--- app/config/environment.js
+++ app/config/environment.js
@@ -20,7 +20,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-        API: 'https://grantsapi.digitalrights.fund'
+        API: 'https://grantsapi.whistleblowingfund.org'
     },
 
     'ember-simple-auth': {
