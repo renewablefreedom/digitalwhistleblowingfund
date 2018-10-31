f734645ccfd226b999af88c6b3965bea597109c2
Christian Muehlhaeuser <muesli@gmail.com>
2018-10-31 15:18:00 +0100

    Moved new public route and outside of proposals

--- app/app/router.js
+++ app/app/router.js
@@ -6,20 +6,26 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
   this.route('admin');
-  this.route('proposals', function() {
-    this.route('new');
-    this.route('show', { path: '/:proposal_id' });
+  this.route('new');
+  this.route('proposals', function () {
+    this.route('show', {
+      path: '/:proposal_id'
+    });
     this.route('all');
     this.route('finished');
     this.route('accepted');
     this.route('rejected');
-    this.route('edit', { path: '/edit/:proposal_id' });
+    this.route('edit', {
+      path: '/edit/:proposal_id'
+    });
     this.route('own');
   });
   this.route('login');
-  this.route('signup', { path: '/signup/:token' });
+  this.route('signup', {
+    path: '/signup/:token'
+  });
 });
 
 export default Router;
